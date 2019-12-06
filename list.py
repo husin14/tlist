a8ab53de69c661f439cd9416331dc7231f2882ee
husin14 <husinsmg@gmail.com>
2019-12-07 06:58:06 +0700

    list

---
+++ list.py
@@ -0,0 +1,22 @@
+# myList = ["hi", 2, "hello"]
+# print (myList)
+# print (myList[0])
+# print (myList[1])
+# print (myList[2])
+# print (myList[0:])
+# print (myList[:2])
+# print ("add the item in the list")
+# get = input("Enter the item: ")
+# myList.append(get)                          
+# print (myList)
+# print ("remove the added item in the list")
+# myList.remove(get)                           
+# print (myList)
+
+
+
+# d = {"name":"Aryan", "age":15, "friends":("abhi", "nitin", "bhasker","budi")}
+# print(d)
+# print(d["name"])
+# print(d["age"])
+# print(d["friends"][:3])
